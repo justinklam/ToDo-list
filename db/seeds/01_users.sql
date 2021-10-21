e5a015b6ed3ea9ea2d6c27c62a0dace41880df72
TJ-Blinn <tjblinn@hotmail.com>
2021-10-21 18:12:20 +0000

    removed comments from schema and seeds, changed last value in todo_lists TABLE to 'is completed'

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -5,22 +5,24 @@ INSERT INTO users (name, email, password, created_at, updated_at) VALUES ('Antho
 INSERT INTO users (name, email, password, created_at, updated_at) VALUES ('Justin', justin@gmail.com, 4);
 INSERT INTO users (name, email, password, created_at, updated_at) VALUES ('Dara', Dara@gmail.com, 5);
 
--- Category IDs are: 1- Films, 2- Restaurants, 3- Books (To Read), 4- Products (To Buy) | All
-INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, current_status) VALUES (1, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
-INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, current_status) VALUES (2, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
-INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, current_status) VALUES (3, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
-INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, current_status) VALUES (4, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
-INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, current_status) VALUES (5, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
+-- These are the 5 categories
+INSERT INTO to_do_categories (category_name) VALUES (Films);
+INSERT INTO to_do_categories (category_name) VALUES (Restaurants);
+INSERT INTO to_do_categories (category_name) VALUES (Books);
+INSERT INTO to_do_categories (category_name) VALUES (Products);
+INSERT INTO to_do_categories (category_name) VALUES (Other);
 
-INSERT INTO to_do_categories (category) VALUES (Films);
-INSERT INTO to_do_categories (category) VALUES (Restaurants);
-INSERT INTO to_do_categories (category) VALUES (Books);
-INSERT INTO to_do_categories (category) VALUES (Products);
-INSERT INTO to_do_categories (category) VALUES (All);
+-- Category IDs are: 1- Films, 2- Restaurants, 3- Books (To Read), 4- Products (To Buy) | Other
+INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, is_completed) VALUES (1, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
+INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, is_completed) VALUES (2, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
+INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, is_completed) VALUES (3, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
+INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, is_completed) VALUES (4, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
+INSERT INTO todo_lists (user_id, category_id, to_do_task, to_do_date, is_completed) VALUES (5, 2, 'Red Lobster', 2021-10-30 20:00:00, TRUE);
 
-INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Can't wait to go down the rabbit hole!", 1, 1);
-INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Meeting after the Quidditch match!", 2, 1);
-INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Remember to confirm reservation", 3, 1);
-INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Meeting before the big game!", 4, 1);
-INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Don't forget to invite friend from work!", 5, 1);
+
+-- INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Can't wait to go down the rabbit hole!", 1, 1);
+-- INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Meeting after the Quidditch match!", 2, 1);
+-- INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Remember to confirm reservation", 3, 1);
+-- INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Meeting before the big game!", 4, 1);
+-- INSERT INTO user_comments (body, user_id, todo_list) VALUES ("Don't forget to invite friend from work!", 5, 1);
 
