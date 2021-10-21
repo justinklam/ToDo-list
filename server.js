b96392e665972fb7ede6fee2d64c5d10143387eb
Justin Lam <justin.kh.lam@gmail.com>
2021-10-21 00:31:11 +0000

    styling changes to app running message

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`ToDo-List app listening on Port: ${PORT} 😈`);
 });
