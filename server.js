d5c9f3a1abae4329e33be642b0eb65d46cc0da91
TJ-Blinn <tjblinn@hotmail.com>
2021-10-24 04:30:49 +0000

    added route for simple login (line 63 app.get) with query for base functionality, login via url

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
-const cookieSession = require('cookie-session');
+const cookieSession = require("cookie-session");
 const morgan = require("morgan");
-const path = require('path');
+const path = require("path");
 
-const database = require('./lib/database');
-const apiRoutes = require('./lib/apiRoutes');
+const database = require("./lib/database");
+const apiRoutes = require("./lib/apiRoutes");
+const dbParams = require("./config/db");
 const app = express();
 
 // PG database client/connection setup - unneeded
@@ -27,10 +28,12 @@ app.use(morgan("dev"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieSession({
-  name: "session",
-  keys: ['key1', 'key2']
-}));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+  })
+);
 
 app.use(
   "/styles",
@@ -46,7 +49,7 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 const apiRouter = express.Router();
 apiRoutes(apiRouter, database);
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -58,11 +61,27 @@ app.use('/api', apiRouter);
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  res.render("index");
+  // req.session.user_id will read the id from cookies
+  const userId = req.session.user_id;
+  if (userId) {
+    // query the database
+    db.query("SELECT * FROM users WHERE id = $1", [userId])
+      .then((data) => {
+        console.log(data.rows[0]);
+
+        //render page with template vars, taken from ejs file, that will show name on homepage once logged in.
+        // (the object that results from data.rows is the object, and we use the name key here)
+        res.render("index", { name: data.rows[0].name });
+      })
+      .catch((error) => {
+        console.log("Error message for login: ", error.message);
+        // res.status()
+      });
+  }
 });
 
 // NodeJS Express to serve static file
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.get("/test", (req, res) => {
   res.send("🤗");
