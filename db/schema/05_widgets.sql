4ffff2baf9f256933b0e63731301e8d3e63e57f6
Justin Lam <justin.kh.lam@gmail.com>
2021-10-26 17:52:24 +0000

    combine 03_todolist items into 02_user_lists, store dated code into 05_widgets. rename 04_categories into 03

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -1,8 +1,29 @@
 -- Drop and recreate Widgets table (Example)
 
-DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
-);
+-- DROP TABLE IF EXISTS widgets CASCADE;
+-- CREATE TABLE widgets (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   user_id INTEGER REFERENCES users(id),
+--   name VARCHAR(255) NOT NULL
+-- );
+
+-- DROP TABLE IF EXISTS todo_lists_items CASCADE;
+
+-- CREATE TABLE todo_lists_items (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   user_list_id INTEGER REFERENCES user_lists(id),
+--   to_do_task VARCHAR(255) NOT NULL,
+--   to_do_date TIMESTAMP NOT NULL,
+--   is_active BOOLEAN NOT NULL DEFAULT TRUE,
+--   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- CREATE TABLE user_comments (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   body TEXT,
+--   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+--   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+--   todo_list INTEGER REFERENCES todo_lists(id) ON DELETE CASCADE
+-- )
