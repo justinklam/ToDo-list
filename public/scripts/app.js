6617250595d327674c535f4eb8928ac624269d4d
Dara Akitoye <daraakitoye1@gmail.com>
2021-10-28 19:43:44 +0000

    Removed duplicate code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -88,6 +88,11 @@ $(document).ready(function () {
   loginToggle();
   registrationToggle();
 
+
+
+
+
+
   // ----------- API SECTION ---------- //
   // Books API
   //values that we will use from the res API (JSON results)
@@ -190,97 +195,3 @@ $(document).ready(function () {
     alert("Search term can not be empty!");
   };
 });
-///Will clean up later
-$(document).ready(function () {
-
-  loginToggle();
-  registrationToggle();
-
-
-  // Displays Books once book-icon is clicked
-
-  const booksSection = function () {
-
-    const HTML = `<div class='header'>Books<div>`
-
-    $('#book-icon').click(function () {
-      $('.header').hide();
-      $('.main_content').append(HTML);
-    })
-    return;
-  }
-  booksSection();
-
-  //Displays shopping list when shopping icon is clicked
-  const shoppingSection = function () {
-
-    const HTML = `<div class='header'>Shopping<div>`
-
-    $('#shopping-icon').click(function () {
-      $('.header').hide();
-      $('.main_content').append(HTML);
-    })
-    return;
-  }
-  shoppingSection();
-
-  // Displays movie lists when movie icon is clicked
-  const moviesSection = function () {
-
-    const HTML = `<div class='header'>Movies<div>`
-
-    $('#movie-icon').click(function () {
-      $('.header').hide();
-      $('.main_content').append(HTML);
-    })
-    return;
-  }
-  moviesSection();
-
-  // Displayes resturant lists when resturant icon is displayed
-  const restaurantSection = function () {
-
-    const HTML = `<div class='header'>Restaurants<div>`
-
-    $('#resturaunt-icon').click(function () {
-      $('.header').hide();
-      $('.main_content').append(HTML);
-    })
-    return;
-  }
-  restaurantSection();
-
-  const homeSection = function () {
-
-    const HTML = `<div class='header'>My Lists<div>`
-
-    $('.home').click(function () {
-      $('.header').hide();
-      $('.main_content').append(HTML);
-    })
-    return;
-  }
-  homeSection();
-})
-
-
-const registrationToggle = function () {
-
-  $('.register').on('click', function () {
-    $('.bg-modal_register').css({ 'visibility': 'visible' })
-  });
-
-  $('button').on('click', function () {
-    $('.bg-modal_register').css({ 'visibility': 'hidden' })
-  })
-}
-
-const loginToggle = function () {
-  $('.login').on('click', function () {
-    $('.bg-modal').css({ 'visibility': 'visible' })
-  });
-
-  $('button').on('click', function () {
-    $('.bg-modal').css({ 'visibility': 'hidden' })
-  })
-}
