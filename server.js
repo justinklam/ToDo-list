96dd8c449a658d1a47a40c88d7546d7cfde95ac2
Justin Lam <justin.kh.lam@gmail.com>
2021-10-28 22:03:37 +0000

    delete 01_user.sql file in seeds, link some of the icons with the routes. reorganize database.js

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ const database = require("./lib/database");
 const apiRoutes = require("./lib/apiRoutes");
 const app = express();
 
-// PG database client/connection setup - unneeded
-// const { Pool } = require("pg");
-// const dbParams = require("./lib/db.js");
-// const db = new Pool(dbParams);
-// db.connect();
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
