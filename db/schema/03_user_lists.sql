87ad23a8deabdd0f68daf22767666c19626b4e36
Justin Lam <justin.kh.lam@gmail.com>
2021-10-29 17:56:53 +0000

    change schema and seeds to to_do_date showing date instead of timestamp

--- db/schema/03_user_lists.sql
+++ db/schema/03_user_lists.sql
@@ -6,7 +6,7 @@ CREATE TABLE user_lists (
   category_id INTEGER REFERENCES todo_categories(id),
   title VARCHAR(255) NOT NULL,
   to_do_task VARCHAR(255) NOT NULL,
-  to_do_date TIMESTAMP NOT NULL,
+  to_do_date DATE NOT NULL,
   is_active BOOLEAN NOT NULL DEFAULT TRUE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
